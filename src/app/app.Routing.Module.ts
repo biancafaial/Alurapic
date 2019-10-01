81f6c7a532a06ad0156717f11854bae75d51114a
biancafaial <biancafaial@gmail.com>
2019-10-01 15:44:11 -0300

    criação da tela de login

--- src/app/app.Routing.Module.ts
+++ src/app/app.Routing.Module.ts
@@ -5,8 +5,13 @@ import { PhotoListComponent } from "./photos/photo-list/photo-list.component";
 import { PhotoFormComponent } from "./photos/photo-form/photo-form.component";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
+import { LoginComponent } from "./home/login/login.component";
 
 const routes: Routes = [
+
+    { 
+        path: '', component: LoginComponent
+     },
     { path: 'user/:userName',
      component: PhotoListComponent,
     resolve:{
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.Routing.Module';
 import { ErrorsModule } from './errors/errors.module';
+import { HomeModule } from './home/home.module';
 
 
 
@@ -17,7 +18,8 @@ import { ErrorsModule } from './errors/errors.module';
     BrowserModule,
     PhotosModule,
     AppRoutingModule,
-     ErrorsModule
+     ErrorsModule,
+     HomeModule
   
   ],
   providers: [],
