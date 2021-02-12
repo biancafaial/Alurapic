5746447e0c41ae63fc008b9ff37cfd04403207cf
biancafaial <biancafaial@gmail.com>
2021-02-12 18:02:26 -0300

    configurando interceptação de token

---
+++ src/app/core/auth/request.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
+import { HttpInterceptor } from "@angular/common/http/src/interceptor";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { TokenService } from "../token/token.service";
+
+@Injectable()
+export class RequestInterceptor implements HttpInterceptor{
+
+    constructor(private tokenService: TokenService){}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if(this.tokenService.hasToken()){
+            const token = this.tokenService.getToken();
+            req = req.clone({
+                setHeaders:{
+                    'x-access-token': token
+                }
+            });
+        }
+      return next.handle(req);
+    }
+}
