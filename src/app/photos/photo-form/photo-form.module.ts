8a8216acd6ab249a6e5f71b38137bb855b348d48
biancafaial <biancafaial@gmail.com>
2021-05-31 10:36:04 -0300

    footer

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { VmessageModule } from 'src/app/shared/components/vmessage/vmessage.module';
 
 @NgModule({
     declarations: [PhotoFormComponent],
 
     imports:[
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        VmessageModule
     ]
 
 })
