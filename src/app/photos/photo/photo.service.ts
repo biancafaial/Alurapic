576ef73eb92a1587cede706c3e095f7a29dab781
Bianca Faial <biancafaial@gmail.com>
2021-06-21 17:50:08 -0300

    parametrizando rotas de fotos selecionadas

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -36,4 +36,8 @@ export class PhotoService{
 
     }
 
+    findById(id: string){
+        return this.http.get<Photo>(API + '/photos/' + id);
+    }
+
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -6,12 +6,18 @@ import { HttpClientModule } from "@angular/common/http";
 import { PhotoFormModule } from "./photo-form/photo-form.module";
 import { PhotoListModule } from "./photo-list/photo-list.module";
 import { PhotoModule } from "./photo/photo.module";
+import { PhotoDetailsModule } from "./photo-details/photo-details.module";
+import { DarkenOnHoverModule } from "../shared/directive/darken-on-hover/darken-on-hover.module";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
     imports: [
         PhotoModule,
         PhotoFormModule,
-        PhotoListModule,         
+        PhotoListModule,  
+        DarkenOnHoverModule,
+        PhotoDetailsModule,
+        RouterModule      
     ]
 })
 export class PhotosModule {
