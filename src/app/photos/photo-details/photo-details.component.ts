21f021023369e251e2e1e21695af3c500355bdce
Bianca Faial <biancafaial@gmail.com>
2021-06-23 17:10:34 -0300

    função de remoção de fotos

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Photo } from '../photo/photo.model';
 import { PhotoService } from '../photo/photo.service';
@@ -17,7 +17,9 @@ export class PhotoDetailsComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private photoService: PhotoService) {
+    private photoService: PhotoService,
+    private router: Router
+    ) {
 
    }
 
@@ -26,5 +28,11 @@ export class PhotoDetailsComponent implements OnInit {
     this.photo$ = this.photoService.findById(this.photoId);
       
   }
+
+  remove() {
+    this.photoService
+        .removePhoto(this.photoId)
+        .subscribe(() => this.router.navigate(['']));
+}
 
 }
