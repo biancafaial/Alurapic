84d828cfec60cea6f62bf06789fa7592e01246d5
Bianca Faial <biancafaial@gmail.com>
2021-06-28 15:53:28 -0300

    manutentenção de layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -36,10 +36,14 @@ const routes: Routes = [
         path: 'p/:photoId', 
         component: PhotoDetailsComponent,
     },
+    { 
+        path: 'not-found', 
+        component: NotFoundComponent
+     },
 
     { 
         path: '**', 
-        component: NotFoundComponent
+        redirectTo:'not-found'
      }
 ];
 
