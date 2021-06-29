f2e3a9d5568defcf1202567d29c95de3154211dd
Bianca Faial <biancafaial@gmail.com>
2021-06-29 17:26:29 -0300

    inserindo barra de progresso

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,6 +1,8 @@
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
 import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { PhotoService } from '../photo/photo.service';
@@ -14,7 +16,8 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
-   preview: string
+  preview: string;
+  percentDone = 0;
 
 
   constructor(
@@ -39,10 +42,20 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService
       .upload(description,allowComments, this.file)
-      .subscribe(()=> {
-        this.alertService.success('Upload completo', true)
-        this.router.navigate(['/user', this.userService.getUserName()])
-      
+      .pipe(finalize(()=>{
+        this.router.navigate(['/user', this.userService.getUserName()]);
+      }))
+      .subscribe((event: HttpEvent<any>)=> {
+        if(event.type == HttpEventType.UploadProgress){
+          this.percentDone = Math.round(100 * event.loaded / event.total)
+        }else if(event instanceof HttpResponse){
+               this.alertService.success('Upload completo', true)
+        }
+         
+      },
+      err =>{
+        console.log(err);
+        this.alertService.danger('Upload Falhou!', true);
       });
       
   }
